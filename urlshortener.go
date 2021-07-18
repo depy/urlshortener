12634a6924b682cf660d51df3147f5975499590a
RDMengineer <matjazvn@gmail.com>
2021-07-18 20:19:08 +0200

    Adds basic functionality with hardcocded 'shortcodes' and redirect urls.

--- urlshortener.go
+++ urlshortener.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func makeHandlersFromMap(mux *http.ServeMux, entries map[string]string) {
+	for shortcode, redirectUrl := range entries {
+		_, err := url.ParseRequestURI(redirectUrl)
+
+		if err != nil {
+			fmt.Println("Skipping", redirectUrl, "=> Error: ", err)
+			continue
+		}
+
+		path := "/" + shortcode
+		mux.Handle(path, http.RedirectHandler(redirectUrl, http.StatusMovedPermanently))
+	}
+}
 
 func main() {
-	fmt.Println("Hello!")
+	entries := make(map[string]string)
+	entries["hello"] = "https://google.com"
+	entries["hi"] = "https://cloudflare.com"
+	entries["heyho"] = "sfg3gGgj;"
+
+	mux := http.NewServeMux()
+	makeHandlersFromMap(mux, entries)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
 }
